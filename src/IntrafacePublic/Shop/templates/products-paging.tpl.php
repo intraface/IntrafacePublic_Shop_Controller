6b52b440ced074521a7ba28f6d814dd844adadce
Lars Olesen <lars@intraface.dk>
2010-12-19 19:52:14 +0100

    Now uses $context->query() instead of $this->GET[]

--- src/IntrafacePublic/Shop/templates/products-paging.tpl.php
+++ src/IntrafacePublic/Shop/templates/products-paging.tpl.php
@@ -1,7 +1,7 @@
 <?php if (!empty($paging['offset']) AND is_array($paging['offset']) AND count($paging['offset']) > 0): ?>
     <p class="paging" style="clear: both;">
     <?php foreach($paging['offset'] as $key => $paging): ?>
-        <?php if ((empty($this->GET['start']) && $paging == 0) || (!empty($this->GET['start']) && $this->GET['start'] == $paging)): ?>
+        <?php if ((!$context->query('start') && $paging == 0) || ($context->query('start') && $context->query('start') == $paging)): ?>
             | <strong><?php e($key + 1); ?></strong>
         <?php else: ?>
             | <a href="<?php e(url('.', array('start' => $paging))); ?>"><?php e($key + 1); ?></a>
